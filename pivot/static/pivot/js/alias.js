3e19acc7b7118e6e20ba72eec242648ef882cce4
mmgionet <mason.gionet@gmail.com>
2018-02-16 14:36:07 -0800

    Refactor myplan major link expections, by storing them in alias.js as a map

--- pivot/static/pivot/js/alias.js
+++ pivot/static/pivot/js/alias.js
@@ -2,7 +2,7 @@
 Ex: If you search CSE, the major corresponding to 'C SCI-0' should
 show up in the search results (in this case: Computer Science on the Seattle Campus) */
 
-const alias = {
+const search_alias = {
 	'ACMS-0': ['AMATH', 'Applied Math', 'Applied and Computational Mathematical Sciences', 'Applied & Computational Mathematical Sciences'],
 	'ACMS-10': ['AMATH', 'Applied Math', 'Applied and Computational Mathematical Sciences', 'Applied & Computational Mathematical Sciences'],
 	'ACMS-20': ['AMATH', 'Applied Math', 'Applied and Computational Mathematical Sciences', 'Applied & Computational Mathematical Sciences'],
@@ -13,4 +13,33 @@ const alias = {
 	'C SCI-10': ['CSE'],
 	'CIV E-0': ['CE'],
 	'IND E-0': ['IE']
+};
+
+
+/* Maps a major without a page in myplan, to the correct page.
+ * Corrects data issues for majors that myplan considers tracts under a parent major. 
+ * Ex: Information Systems 'I S' is a Business Major at UW Seattle, although considered
+ * a track under business in Myplan, thus the link we create should direct to the parent major major
+ * not an error page because the myplan major page does not exist.
+ */
+
+// Define constants in case a common link changes
+var seattle_business = ['BUS'];
+
+var tacoma_business = ['T BUS'];
+
+const myplan_alias = {
+	'I S': seattle_business,
+	'ACCTG': seattle_business,
+	'ENTRE': seattle_business,
+	'B A': seattle_business,
+	'FINANC': seattle_business,
+	"HRMGT": seattle_business,
+	'MKTG': seattle_business,
+	'OSCM': seattle_business,
+	'B BUSX': ['B BUS'],
+	'T ACCT': tacoma_business,
+	'T FIN': tacoma_business,
+	'T MGMT': tacoma_business,
+	'T MKTG': tacoma_business,
 };
