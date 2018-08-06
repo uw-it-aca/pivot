b6010f5005f19b188791b2507d91948ba6a3edc2
Craig M. Stimmel <cstimmel@uw.edu>
2018-08-06 13:53:46 -0700

    Don't name your module csv when you're importing Python's csv.

--- pivot/tests.py
+++ pivot/tests.py
@@ -3,5 +3,5 @@ Tests for the pivot app.
 """
 
 from pivot.test.context import ContextProcessorTest
-from pivot.test.csv import CsvDataApiTest
+from pivot.test.csvapi import CsvDataApiTest
 from pivot.test.misc import PivotConfigTest
