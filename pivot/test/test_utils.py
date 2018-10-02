d511d34b47ba1a40019771ca5529643fe3b06ab7
Aditya <adikumar@uw.edu>
2018-10-02 14:05:11 -0700

    added utilities for figuring out the most recent term we have data for
    this is better that checking which term it actually is, since we may not have data for that term

---
+++ pivot/test/test_utils.py
@@ -0,0 +1,18 @@
+"""
+Tests utility scripts
+"""
+
+from django.test import TestCase, RequestFactory
+import pivot
+from pivot.utils import *
+
+
+TEST_CSV_PATH = os.path.join(os.path.dirname(pivot.__file__),
+                             'test',
+                             'test_resources',
+                             'csvfiles/',)
+
+class UtilsTest(TestCase):
+    def test_google_analytics_processor(self):
+        with self.settings(CSV_ROOT=TEST_CSV_PATH):
+            self.assertEquals(get_latest_term(), 'au12')
