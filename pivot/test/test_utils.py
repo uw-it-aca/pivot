8c3790e6074819d0513eeb1690f8f2a6c69c8f96
Keith Roberts <keithrob@uw.edu>
2019-03-01 13:22:57 -0800

    Added test coverage for templatetags and additional case for utils

--- pivot/test/test_utils.py
+++ pivot/test/test_utils.py
@@ -7,7 +7,8 @@ from django.test import TestCase, RequestFactory
 from django.test.utils import override_settings
 
 import pivot
-from pivot.utils import get_latest_term
+from pivot.utils import get_latest_term, is_more_recent
+from pivot.templatetags.pivot_extras import year_select_tab
 
 
 TEST_CSV_PATH = os.path.join(os.path.dirname(pivot.__file__),
@@ -20,3 +21,20 @@ class UtilsTest(TestCase):
     @override_settings(CSV_ROOT=TEST_CSV_PATH)
     def test_get_latest_term(self):
         self.assertEquals(get_latest_term(), 'au12')
+
+    @override_settings(CSV_ROOT=TEST_CSV_PATH)
+    def test_is_more_recent_true(self):
+        self.assertTrue(is_more_recent('au19', 'au18'))
+
+    @override_settings(CSV_ROOT=TEST_CSV_PATH)
+    def test_pivot_extras(self):
+        template = """
+        <a href=".?num_qtrs={0}&end_yr={4}&end_qtr={2}">
+            <strong>Last {1} Years</strong> <br>
+            <span>
+                {2}{3} - {2}{4}
+            </span>
+        </a>
+    """.format(8, 2, 'AU', 10, 12)
+        html = year_select_tab(8)
+        self.assertEqual(html, template)
