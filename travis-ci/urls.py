c9837ca12fc084a37fc3eebbb5f02feb1a939374
Sarthak Singh <ss269@uw.edu>
2019-12-06 12:25:36 -0800

    Fixed travis urls.py

--- travis-ci/urls.py
+++ travis-ci/urls.py
@@ -3,9 +3,9 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    url(r'^login/$', auth_views.LoginView, {'template_name': 'login.html'},
+    url(r'^login/$', auth_views.LoginView.as_view(), {'template_name': 'login.html'},
         name='login'),
-    url(r'^logout/$', auth_views.LogoutView, {'next_page': '/'},
+    url(r'^logout/$', auth_views.LogoutView.as_view(), {'next_page': '/'},
         name='logout'),
     url(r'^', include('pivot.urls')),
 ]
