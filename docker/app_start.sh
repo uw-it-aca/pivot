7358143ec16b1fc9f255bdd1ce8a36f7825c5f65
DDaiJ <daij7@uw.edu>
2020-06-26 12:25:49 -0700

    superuser

--- docker/app_start.sh
+++ docker/app_start.sh
@@ -2,4 +2,4 @@
 
 python manage.py makemigrations
 python manage.py migrate
-# python manage.py initadmin --username test1 --password test1234 --noinput --email 'abcdef@gmail.com'
+python manage.py initadmin --username test1 --password test1234 --noinput --email 'abcdef@gmail.com'
