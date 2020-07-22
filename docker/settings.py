eadb01038adfe1149b1ef6cc2099f4898ae6689a
DDaiJ <daij7@cs.washington.edu>
2020-07-22 16:00:34 -0700

    change settings

--- docker/settings.py
+++ docker/settings.py
@@ -17,14 +17,6 @@ INSTALLED_APPS += [
     'django_user_agents',
 ]
 
-
-WEBPACK_LOADER = {
-    'DEFAULT': {
-        'BUNDLE_DIR_NAME': 'pivot/bundles/',
-        'STATS_FILE': os.path.join(BASE_DIR, 'pivot', 'static', 'webpack-stats.json'),
-    }
-}
-
 CSV_ROOT = os.path.join(BASE_DIR, "data/")
 STATIC_ROOT = 'static/'
 COMPRESS_ROOT = 'static/'
