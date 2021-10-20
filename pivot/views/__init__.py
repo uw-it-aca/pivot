78e73fc29b0687da57a31878a92ab4613e23c446
Jim Laney <jlaney@uw.edu>
2021-10-20 16:03:44 -0700

    add license/copyright to src files

--- pivot/views/__init__.py
+++ pivot/views/__init__.py
@@ -1,3 +1,6 @@
+# Copyright 2021 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
 from django.conf import settings
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
