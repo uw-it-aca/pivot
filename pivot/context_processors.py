255a75fc18231a82974c707509e2a5e6f626d297
Craig M. Stimmel <cstimmel@uw.edu>
2022-01-08 15:40:13 -0800

    Reformat with black

--- pivot/context_processors.py
+++ pivot/context_processors.py
@@ -5,15 +5,13 @@ from django.conf import settings
 
 
 def support_email(request):
-    return {'support_email': getattr(settings, 'SUPPORT_EMAIL', '')}
+    return {"support_email": getattr(settings, "SUPPORT_EMAIL", "")}
 
 
 def google_analytics(request):
-    ga_key = getattr(settings, 'GOOGLE_ANALYTICS_KEY', False)
-    return {
-        'GOOGLE_ANALYTICS_KEY': ga_key,
-        'google_analytics': ga_key
-    }
+    ga_key = getattr(settings, "GOOGLE_ANALYTICS_KEY", False)
+    return {"GOOGLE_ANALYTICS_KEY": ga_key, "google_analytics": ga_key}
+
 
 def show_alert(request):
-    return {'show_alert': settings.SHOW_ALERT}
+    return {"show_alert": settings.SHOW_ALERT}
